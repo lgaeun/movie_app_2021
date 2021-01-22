ff9e70c81e973118c96495fe11ae6d78998548ca
lgaeun <gaeungaeun4837@gmail.com>
2021-01-22 13:50:35 +0900

    jsx&prop까지 완료

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PropTypes from "prop-types";
+ //props
+function Food({name, picture, rating}){
+  
+  return <div>
+    <h2> I like {name} </h2>
+    <h4> I got {rating}/6.0</h4>
+  </div>
+}
+//component = html을 리턴하는 function
+
+//descsription of props that i want to get
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  rating: PropTypes.number // number or undefined 만 허용 
+}
+
+const foodILike = [
+  {
+    id:1,
+    name: "kimchi",
+    rating: 4.1
+
+  },
+  {
+    id: 2,
+    name: "kimbap",
+    rating: 2
+  },
+  {
+    id:3,
+    name: "sushi",
+    rating: 5.6
+  },
+  {
+    id:4,
+    name: "pissa",
+    rating: 4.3
+  },
+]
+
+function renderFood(dish){
+  // console.log(dish);
+  return <Food name = {dish.name} />
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* {foodILike.map(renderFood)} */}
+      {foodILike.map(dish => (
+        <Food 
+        key={dish.id} 
+        name={dish.name} 
+        rating={dish.rating} />
+      ))}
     </div>
   );
 }
+
 
 export default App;
